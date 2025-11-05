1a29b7ad46713a138694bebda829169b20080be2
Ryanjawa <corazon3663@gmail.com>
2025-11-05 20:50:50 +0700

    standart coding

---
+++ config/variables.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "creatorName" => "RyanJr-kun",
+    "creatorUrl" => "https://github.com/RyanJr-kun",
+    "templateName" => "PRIMA",
+    "templateSuffix" => "PRIMA Bootstrap Dashboard Template",
+    "templateVersion" => "1.0.0",
+];
